c7532d069bd066c9deaadb315f54dffcedea7c1e
kvdmitrieva <katedv02@gmail.com>
2023-11-07 14:31:50 +0300

    fix speaker id extraction

--- hw_ss/datasets/mixture_dataset.py
+++ hw_ss/datasets/mixture_dataset.py
@@ -83,7 +83,7 @@ class MixtureDataset(BaseDataset):
         mixes = sorted(glob(os.path.join(self._mixture_dir, '*-mixed.wav')))
         targets = sorted(glob(os.path.join(self._mixture_dir, '*-target.wav')))
 
-        target_speakers = list(set([int(path.split('_')[0]) for path in targets]))
+        target_speakers = list(set([int(path.split('/')[-1].split('_')[0]) for path in targets]))
         self._speaker_dict = {speaker: i for i, speaker in enumerate(target_speakers)}
 
         for ref, mix, target in tqdm(
