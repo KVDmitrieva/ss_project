d629f7fad791849710ad14bd5e4896f14fe2e9af
kvdmitrieva <katedv02@gmail.com>
2023-11-07 13:13:00 +0300

    add default args

--- hw_ss/metric/pesq_metric.py
+++ hw_ss/metric/pesq_metric.py
@@ -5,7 +5,7 @@ from hw_ss.base.base_metric import BaseMetric
 
 
 class PESQMetric(BaseMetric):
-    def __init__(self, fs, mode, *args, **kwargs):
+    def __init__(self, fs=16000, mode="wb", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pesq = PerceptualEvaluationSpeechQuality(fs, mode)
 
--- hw_ss/metric/stoi_metric.py
+++ hw_ss/metric/stoi_metric.py
@@ -5,7 +5,7 @@ from hw_ss.base.base_metric import BaseMetric
 
 
 class STOIMetric(BaseMetric):
-    def __init__(self, fs, *args, **kwargs):
+    def __init__(self, fs=16000, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stoi = ShortTimeObjectiveIntelligibility(fs)
 
