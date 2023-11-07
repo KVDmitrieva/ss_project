b0d73dae78162478280b18b61a6b7ae997bd7444
kvdmitrieva <katedv02@gmail.com>
2023-11-07 14:50:33 +0300

    fix multi task loss

--- hw_ss/loss/MultiTaskLossWrapper.py
+++ hw_ss/loss/MultiTaskLossWrapper.py
@@ -1,3 +1,4 @@
+import torch
 from torch import Tensor
 
 from hw_ss.base.base_loss import BaseLoss
@@ -6,20 +7,21 @@ from hw_ss.loss.CELossWrapper import CELossWrapper
 
 
 class MultiTaskLossWrapper(BaseLoss):
-    def __init__(self):
+    def __init__(self, alphas, gamma):
         super().__init__()
         self.cross_entropy = CELossWrapper()
         self.si_sdr = SISDRLossWrapper()
+        self.alphas = alphas
+        self.gamma = gamma
 
-    def forward(self,  signals, log_probs, alphas, gamma, target=None, speaker=None, **batch) -> Tensor:
-        si_sdr_loss = 0.
+    def forward(self,  signals, log_probs, target=None, speaker=None, **batch) -> Tensor:
+        si_sdr_loss = torch.zeros_like(signals[:, 0])
         for i in range(signals.shape[0]):
-            alpha = 1 - sum(alphas) if i == 0 else alphas[i - 1]
+            alpha = 1 - sum(self.alphas) if i == 0 else self.alphas[i - 1]
             si_sdr_loss += alpha * self.si_sdr(signals[:, i], target)
 
         if log_probs is not None:
             ce_loss = self.cross_entropy(log_probs, speaker)
-            return si_sdr_loss + gamma * ce_loss
+            return si_sdr_loss + self.gamma * ce_loss
 
         return si_sdr_loss
-
