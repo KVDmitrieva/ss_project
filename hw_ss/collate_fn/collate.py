b8e271d93bd5fcd7dda590972bd9db8e15f59ecc
kvdmitrieva <katedv02@gmail.com>
2023-11-07 19:04:16 +0300

    more debug

--- hw_ss/collate_fn/collate.py
+++ hw_ss/collate_fn/collate.py
@@ -32,7 +32,9 @@ def collate_fn(dataset_items: List[dict]):
 
     audio_target = pad_sequence(audio + target, batch_first=True)
     audio, target = audio_target[:len(dataset_items)].transpose(1, 2), audio_target[len(audio_target):].transpose(1, 2)
+    print("COLLATE DEBUG", target.shape)
     target = target.squeeze(1)
+    print("COLLATE DEBUG", target.shape)
 
     return {
         "text": text,
