152ad4708a7170f5d2ace4dab3e4a549d0a76e8d
kvdmitrieva <katedv02@gmail.com>
2023-11-08 14:53:57 +0300

    bug fix

--- hw_ss/metric/pesq_metric.py
+++ hw_ss/metric/pesq_metric.py
@@ -7,9 +7,10 @@ from hw_ss.base.base_metric import BaseMetric
 class PESQMetric(BaseMetric):
     def __init__(self, fs=16000, mode="wb", *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pesq = PerceptualEvaluationSpeechQuality(fs, mode)
+        self.pesq = PerceptualEvaluationSpeechQuality(fs, mode, on_error=1)
 
     def __call__(self, signal: Tensor, target: Tensor, **kwargs):
         signal = signal.cpu().detach()
         target = target.cpu().detach()
         return self.pesq(signal, target)
+
