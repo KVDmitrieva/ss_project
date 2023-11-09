cd1a353421a7566596b6cc335aab37184c9310c2
kvdmitrieva <katedv02@gmail.com>
2023-11-09 15:24:33 +0300

    custom si-sdr -> torchmetrics

--- hw_ss/metric/si_sdr_metric.py
+++ hw_ss/metric/si_sdr_metric.py
@@ -1,14 +1,15 @@
 from torch import Tensor
 
 from hw_ss.base.base_metric import BaseMetric
-from hw_ss.metric.utils import calc_si_sdr
+from torchmetrics import ScaleInvariantSignalDistortionRatio
 
 
 class SISDRMetric(BaseMetric):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.si_sdr = ScaleInvariantSignalDistortionRatio()
 
     def __call__(self, signal: Tensor, target: Tensor, **kwargs):
         signal = signal.cpu().detach()
         target = target.cpu().detach()
-        return calc_si_sdr(signal, target)
+        return self.si_sdr(signal, target)
--- hw_ss/tests/test_metrics.py
+++ hw_ss/tests/test_metrics.py
@@ -1,27 +1,12 @@
 import torch
 import unittest
 
-from torchmetrics.audio import SignalDistortionRatio, ScaleInvariantSignalDistortionRatio
+from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
 
-from hw_ss.metric.sdr_metric import SDRMetric
-from hw_ss.metric.si_sdr_metric import SISDRMetric
+from hw_ss.metric.utils import calc_si_sdr
 
 
 class TestMetrics(unittest.TestCase):
-    def test_sdr_metric(self):
-        torch.manual_seed(1)
-
-        target = torch.randn(1000)
-        preds = torch.randn(1000)
-
-        torch_sdr = SignalDistortionRatio()
-        custom_sdr = SDRMetric()
-
-        torch_result = torch_sdr(preds, target)
-        custom_result = custom_sdr(preds, target)
-
-        self.assertAlmostEqual(custom_result.item(), torch_result.item(), places=2)
-
     def test_si_sdr_metric(self):
         torch.manual_seed(1)
 
@@ -29,10 +14,9 @@ class TestMetrics(unittest.TestCase):
         preds = torch.randn(5)
 
         torch_si_sdr = ScaleInvariantSignalDistortionRatio()
-        custom_si_sdr = SISDRMetric()
 
         torch_result = torch_si_sdr(preds, target)
-        custom_result = custom_si_sdr(preds, target)
+        custom_result = calc_si_sdr(preds, target)
 
         self.assertAlmostEqual(torch_result.item(), custom_result.item(), places=2)
 
@@ -43,9 +27,8 @@ class TestMetrics(unittest.TestCase):
         preds = torch.randn(4, 1000)
 
         torch_si_sdr = ScaleInvariantSignalDistortionRatio()
-        custom_si_sdr = SISDRMetric()
 
         torch_result = torch_si_sdr(preds, target)
-        custom_result = custom_si_sdr(preds, target)
+        custom_result = calc_si_sdr(preds, target)
 
         self.assertAlmostEqual(torch_result.item(), custom_result.item(), places=2)
