f00eb2536c49f6170bac3447300464792e27ba65
kvdmitrieva <katedv02@gmail.com>
2023-11-12 21:07:42 +0300

    custom dir for triplet data

--- hw_ss/datasets/custom_audio_dataset.py
+++ hw_ss/datasets/custom_audio_dataset.py
@@ -1,7 +1,7 @@
 import logging
-from pathlib import Path
 
 import torchaudio
+from tqdm import tqdm
 
 from hw_ss.base.base_dataset import BaseDataset
 
@@ -10,13 +10,20 @@ logger = logging.getLogger(__name__)
 
 class CustomAudioDataset(BaseDataset):
     def __init__(self, data, *args, **kwargs):
-        index = data
-        for entry in data:
-            assert "path" in entry
-            assert Path(entry["path"]).exists(), f"Path {entry['path']} doesn't exist"
-            entry["path"] = str(Path(entry["path"]).absolute().resolve())
-            entry["text"] = entry.get("text", "")
-            t_info = torchaudio.info(entry["path"])
-            entry["audio_len"] = t_info.num_frames / t_info.sample_rate
+        index = []
+        for ref, mix, target in tqdm(data, desc="Preparing mixture triplets"):
+            t_info = torchaudio.info(mix)
+            length = t_info.num_frames / t_info.sample_rate
+            index.append(
+                {
+                    "path": mix,
+                    "ref_path": ref,
+                    "target_path": target,
+                    "text": "",
+                    "audio_len": length,
+                    "speaker": -1,
+                    "speaker_id": -1
+                }
+            )
 
         super().__init__(index, *args, **kwargs)
--- hw_ss/datasets/custom_dir_audio_dataset.py
+++ hw_ss/datasets/custom_dir_audio_dataset.py
@@ -1,5 +1,11 @@
+import os
 import logging
 from pathlib import Path
+from glob import glob
+
+from tqdm import tqdm
+
+import torchaudio
 
 from hw_ss.datasets.custom_audio_dataset import CustomAudioDataset
 
@@ -7,17 +13,12 @@ logger = logging.getLogger(__name__)
 
 
 class CustomDirAudioDataset(CustomAudioDataset):
-    def __init__(self, audio_dir, transcription_dir=None, *args, **kwargs):
-        data = []
-        for path in Path(audio_dir).iterdir():
-            entry = {}
-            if path.suffix in [".mp3", ".wav", ".flac", ".m4a"]:
-                entry["path"] = str(path)
-                if transcription_dir and Path(transcription_dir).exists():
-                    transc_path = Path(transcription_dir) / (path.stem + '.txt')
-                    if transc_path.exists():
-                        with transc_path.open() as f:
-                            entry["text"] = f.read().strip()
-            if len(entry) > 0:
-                data.append(entry)
+    def __init__(self, audio_dir, *args, **kwargs):
+        audio_dir = Path(audio_dir)
+        refs = sorted(glob(os.path.join(audio_dir / "refs", '*-ref.wav')))
+        mixes = sorted(glob(os.path.join(audio_dir / "mix", '*-mixed.wav')))
+        targets = sorted(glob(os.path.join(audio_dir / "targets", '*-target.wav')))
+
+        data = list(zip(refs, mixes, targets))
+
         super().__init__(data, *args, **kwargs)
